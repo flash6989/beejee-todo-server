c984830534114eda64368ed43ea960a31b89f411
flash69891 <flash6989@yandex.ru>
2022-10-17 15:41:06 +0400

    fix: registration route

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,6 @@ router.post('/changetask', checkRoleMiddleware('admin'), changeTask)
 router.post('/changestatustask', checkRoleMiddleware('admin'), changeStatusTask)
 router.post('/login', login)
 router.get('/checkauth', checkAuth)
-router.post('/registration', [
-  check('login', 'Логин не может быть пустым').notEmpty(),
-  check('password', 'Пароль должен быть от 3 до 10 символов').isLength({ min: 3, max: 10 })
-], registration)
+router.post('/registration', registration)
 
 module.exports = router
